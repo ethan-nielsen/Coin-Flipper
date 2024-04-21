a88681da2d43994c220bceb74c9b1e4864786e59
Ethan Nielsen <73164575+ethan-nielsen@users.noreply.github.com>
2024-04-20 21:11:16 -0600

    debug false

--- server.py
+++ server.py
@@ -56,4 +56,4 @@ def display_result():
                            bankroll=session.get('bankroll', 1000))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
