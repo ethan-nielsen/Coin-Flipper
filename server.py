ab6acf7d3fb48b17998f5d9d7c5d7cbcb460dad0
Ethan Nielsen <73164575+ethan-nielsen@users.noreply.github.com>
2024-07-11 12:33:43 -0600

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ import time
 app = Flask(__name__)
 app.secret_key = '12345'
 
-# GPIO chip and pin, raspberry pi5
 chip = gpiod.Chip('gpiochip4')
 relay_pin = 17
 relay_line = chip.get_line(relay_pin)
@@ -22,29 +21,27 @@ def trigger_relay():
 @app.route('/bet', methods=['GET', 'POST'])
 def bet():
     if 'bankroll' not in session:
-        session['bankroll'] = 1000
-
+        session['bankroll'] = 1000  
+        
     if request.method == 'POST':
         bet_amount = int(request.form.get('bet_amount', 0))
-        bet_on = request.form.get('bet_on', '')
-
+        bet_on = request.form.get('bet_on', '')      
+        
         if not bet_amount or not bet_on:
             return render_template('bet.html', error="Please fill all fields correctly.", bankroll=session['bankroll'])
-
         session['bet_amount'] = bet_amount
         session['bet_on'] = bet_on
-        session['result'] = random.choice(['Heads', 'Tails'])
-
-        # Check the result of the bet and update the bankroll
+        session['result'] = random.choice(['Heads', 'Tails'])  
+        
         if session['result'] == session['bet_on']:
             session['bankroll'] += bet_amount
             session['result_text'] = "You won!"
         else:
             session['bankroll'] -= bet_amount
-            session['result_text'] = f"You lost ${bet_amount}!"
-
+            session['result_text'] = f"You lost ${bet_amount}!"     
+        
         return redirect(url_for('flip_coin'))
-
+        
     return render_template('bet.html', bankroll=session['bankroll'])
 
 @app.route('/trigger-relay', methods=['POST'])
@@ -66,15 +63,11 @@ def display_result():
 @app.route('/top-up', methods=['GET', 'POST'])
 def top_up():
     if request.method == 'POST':
-        # Handle the form submission to update the bankroll
         amount = int(request.form.get('top_up_amount', 0))
         if amount > 0:
             session['bankroll'] += amount
-        return redirect(url_for('bet'))  # Redirect to the bet page after topping up
-
-    # If it's a GET request render the top-up page
+        return redirect(url_for('bet'))
     return render_template('top_up.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
